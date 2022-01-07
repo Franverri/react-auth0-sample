9e6d74e65f592feb2d43322b304c1a961450152c
Franverri <francoetche@gmail.com>
2022-01-07 16:11:45 -0300

    Login/logout with auth0 + profile view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import AuthenticationButton from "./components/authentication-button";
+import { useAuth0 } from '@auth0/auth0-react';
+
+import Profile from "./views/profile";
+
 function App() {
+  const { isLoading } = useAuth0();
+
+  if(isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
-    <div>
+    <>
       React Auth0 Sample
-    </div>
+      <AuthenticationButton />
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
